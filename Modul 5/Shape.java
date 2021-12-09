d39a06e4ef230d94c6849b781ab45d7649be9332
SariKrisdianto <92960587+SariKrisdianto@users.noreply.github.com>
2021-12-09 18:57:43 +0700

    Create Shape.java

---
+++ Modul 5/Shape.java
@@ -0,0 +1,16 @@
+public class Shape {
+	Shape(){ }
+
+	public void draw( int d) {
+		draw= d;
+	}
+	public void erase( int e) {
+		erase= e;
+	}
+	public void move( int m) {
+		open= m;
+	}
+	public void resize( int r) {
+		resize= r;
+	}
+}
