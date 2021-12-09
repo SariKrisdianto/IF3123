f4f887d75fcdc41c7152960048de6c4837c14d28
SariKrisdianto <92960587+SariKrisdianto@users.noreply.github.com>
2021-12-09 18:55:50 +0700

    Create Window.java

---
+++ Modul 5/Window.java
@@ -0,0 +1,19 @@
+public class Window {
+	Window(){ }
+
+	public void open( int o) {
+		open= o;
+	}
+	public void close( int c) {
+		close= c;
+	}
+	public void move( int m) {
+		open= o;
+	}
+	public void display( int d) {
+		display= d;
+	}
+	public void handle( event h) {
+		handle= h;
+	}
+}
